857176838f51f2fa6a9ca7d631f365eb45ce1c2c
tesmen <tezmo@mail.ru>
2016-10-21 01:37:38 +0300

    init

---
+++ index.php
@@ -0,0 +1,47 @@
+<meta charset="utf-8">
+
+<script src="jquery-3.1.0.min.js"></script>
+<script src="bootstrap.js"></script>
+<link rel="stylesheet" type="text/css" href="bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="bootstrap-theme.css"/>
+<link rel="stylesheet" type="text/css" href="style.css"/>
+
+<div class="container">
+    <div class="services col-md-12">
+        <a class="item">Объявления</a>
+        <a class="item">Помощь</a>
+    </div>
+
+    <div class="col-xs-12 hidden-xs v-header">
+        <div class="col-xs-4 hidden-xs v-logo">v-logo</div>
+        <div class="col-xs-4 hidden-xs v-about">v-about</div>
+        <div class="col-xs-4 hidden-xs v-user-bar">v-user-bar</div>
+    </div>
+
+    <div class="spacer-50 col-xs-12"></div>
+
+    <div class="col-xs-12 hidden-xs v-nav ">
+        <ul class="nav nav-pills nav-justified">
+            <li class="presentation hidden-xs v-logo active"><a href="#">Catalog</a></li>
+            <li class="presentation hidden-xs v-about"><a href="#">Masters</a></li>
+            <!--            <li class="col-xs-3 hidden-xs v-user-bar"><a href="#">Places</a></li>-->
+            <li class="presentation hidden-xs v-user-bar"><a href="#">Routes</a></li>
+            <!--            <li class="col-xs-2 hidden-xs v-user-bar"><a href="#">Bonuses</a></li>-->
+        </ul>
+    </div>
+
+    <div class="spacer-50 col-xs-12"></div>
+
+    <div class="col-xs-3 hidden-xs">
+        <ul class="nav nav-pills nav-stacked">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Messages</a></li>
+        </ul>
+    </div>
+    <div class="col-xs-9 content">
+
+    </div>
+
+
+</div>
