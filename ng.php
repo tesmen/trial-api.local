3c2b43bfed4fb5fd4a9e4089915af56e543dcba5
tesmen <tezmo@mail.ru>
2016-12-17 15:33:45 +0300

    init

--- ng.php
+++ ng.php
@@ -1,21 +1,31 @@
 <meta charset="utf-8">
-
-<!--<script src="jquery-3.1.0.min.js"></script>-->
-<!--<script src="bootstrap.js"></script>-->
 <script src="angular/angular.js"></script>
-<script src="angular/angular-route.js"></script>
-<script src="angular/controllers.js"></script>
+<script src="angular-notify.js"></script>
+<script src="iq-notify.js"></script>
+<link rel="stylesheet" href="notify.css">
 
+<script>
+    angular.module('myApp', ['cgNotify', 'iqNotify'])
+        .controller('ctrl', function ($scope,$rootScope, notify, facc) {
+            $rootScope.i18nCache = {};
 
-<div ng-app="myApp">
-    <div class="nav navbar">
-        <a href="#red">Red</a>
-        <a href="#blue">Blue</a>
+            $rootScope.i18n = function(){
+                return $rootScope.i18nCache
+            };
+
+            $scope.getIt = function () {
+                $rootScope.i18nCache[$scope.keeey] = $scope.vaaal;
+                console.log($rootScope.i18nCache[$scope.keeey]);
+            }
+        })
+    ;
+</script>
+
+<div ng-app="myApp" ng-cloak>
+    <div ng-controller="ctrl">
+        <input type="text" ng-model="keeey" placeholder="key">
+        <input type="text" ng-model="vaaal">
+        <button ng-click="getIt()">load</button>
+        <span> {{ i18n() | json }}</span>
     </div>
-
-    <ng-view></ng-view>
-
-    <script type="text/ng-template" id="blue.htm">
-        blue
-    </script>
 </div>
