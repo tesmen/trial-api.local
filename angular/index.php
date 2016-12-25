34270fff1b1b4581b54cde418ef52b310642923d
tesmen <tezmo@mail.ru>
2016-12-25 22:39:30 +0300

    fix

--- angular/index.php
+++ angular/index.php
@@ -4,27 +4,16 @@
 <link rel="stylesheet" href="notify.css">
 
 <script>
-    angular.module('myApp', ['cgNotify', 'iqNotify'])
-        .controller('ctrl', function ($scope,$rootScope, notify, facc) {
-            $rootScope.i18nCache = {};
-
-            $rootScope.i18n = function(){
-                return $rootScope.i18nCache
-            };
-
-            $scope.getIt = function () {
-                $rootScope.i18nCache[$scope.keeey] = $scope.vaaal;
-                console.log($rootScope.i18nCache[$scope.keeey]);
-            }
+    angular.module('myApp', [])
+        .controller('ctrl', function ($scope) {
+            $scope.text = 'Hello';
         })
     ;
 </script>
 
 <div ng-app="myApp" ng-cloak>
     <div ng-controller="ctrl">
-        <input type="text" ng-model="keeey" placeholder="key">
-        <input type="text" ng-model="vaaal">
-        <button ng-click="getIt()">load</button>
-        <span> {{ i18n() | json }}</span>
+        {{ text }}
     </div>
+
 </div>
